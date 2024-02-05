95e623f66524e2df63456e087412cdb79a878716
tttuuu13 <tim.ib.222005@gmail.com>
2024-02-06 02:20:24 +0300

    initial

---
+++ components/card.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export const Card = ({frontText, backText}) => {
+  return(
+    <View>
+      <Text>{frontText}</Text>
+    </View>
+  )
+}
